cb36d3a4a16e454ea6de993df12114a1cafd96cb
Eduardo Meneses <43613311+edrimd@users.noreply.github.com>
2021-03-31 11:22:39 -0500

    Componentes tabla

---
+++ src/views/Bitacoras.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="font-semibold uppercase mb-4">Gestión de bitácoras</h1>
+        <hr class="mb-4">
+        <form action="" class="text-xs">
+            <input type="seach" placeholder="Buscar bitácora" class="p-2 bg-gray-100 rounded mr-4">
+            <button class="p-2 rounded bg-blue-100 text-blue-500">Buscar</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style scoped>
+
+</style>
